d41abdb2a81040791e8446a7e913bb84502daf7e
Matthew Perry <thesedatedprince@gmail.com>
2017-04-08 15:06:49 +0100

    seeded card name data

--- spec/features/cards_feature_spec.rb
+++ spec/features/cards_feature_spec.rb
@@ -11,5 +11,110 @@ feature 'cards' do
       visit '/cards'
       expect(page).to have_content "The Fool"
     end
+
+    scenario 'magician card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Magician"
+    end
+
+    scenario 'priestess card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The High Priestess"
+    end
+
+    scenario 'empress card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Empress"
+    end
+
+    scenario 'empreror card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Emperor"
+    end
+
+    scenario 'hierophant card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Hierophant"
+    end
+
+    scenario 'lovers card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Lovers"
+    end
+
+    scenario 'chariot card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Chariot"
+    end
+
+    scenario 'strength card should be present' do
+      visit '/cards'
+      expect(page).to have_content "Strength"
+    end
+
+    scenario 'hermit card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Hermit"
+    end
+
+    scenario 'fortuna card should be present' do
+      visit '/cards'
+      expect(page).to have_content "Wheel of Fortune"
+    end
+
+    scenario 'justice card should be present' do
+      visit '/cards'
+      expect(page).to have_content "Justice"
+    end
+
+    scenario 'hanged man card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Hanged Man"
+    end
+
+    scenario 'death card should be present' do
+      visit '/cards'
+      expect(page).to have_content "Death"
+    end
+
+    scenario 'temperance card should be present' do
+      visit '/cards'
+      expect(page).to have_content "Temperance"
+    end
+
+    scenario 'devil card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Devil"
+    end
+
+    scenario 'tower card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Tower"
+    end
+
+    scenario 'star card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Star"
+    end
+
+    scenario 'moon card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Moon"
+    end
+
+    scenario 'sun card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The Sun"
+    end
+
+    scenario 'judgement card should be present' do
+      visit '/cards'
+      expect(page).to have_content "Judgement"
+    end
+
+    scenario 'world card should be present' do
+      visit '/cards'
+      expect(page).to have_content "The World"
+    end
   end
 end
